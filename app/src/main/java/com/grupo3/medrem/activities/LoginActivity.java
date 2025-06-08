a7531a8f7df99ccf87e8439a325dedbc040e7e63
Homero Percy Quispe Coaquira <percy91@gmail.com>
2025-06-08 04:03:56 -0500

    LoginActivity agregado

--- app/src/main/java/com/grupo3/medrem/activities/LoginActivity.java
+++ app/src/main/java/com/grupo3/medrem/activities/LoginActivity.java
@@ -1,8 +1,6 @@
 package com.grupo3.medrem.activities;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,22 +10,17 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.grupo3.medrem.R;
 
-public class Onboarding4Activity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_onboarding4);
+        setContentView(R.layout.activity_login);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
-
-    public void onClickLogin(View view){
-        Intent intent = new Intent(this, LoginActivity.class);
-        startActivity(intent);
     }
 }
