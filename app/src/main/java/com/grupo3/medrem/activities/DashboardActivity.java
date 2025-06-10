de1641b1c7da906a90152eee009b0dddf80baea6
Homero Percy Quispe Coaquira <percy91@gmail.com>
2025-06-09 22:42:41 -0500

    RegisterActivity

--- app/src/main/java/com/grupo3/medrem/activities/DashboardActivity.java
+++ app/src/main/java/com/grupo3/medrem/activities/DashboardActivity.java
@@ -1,8 +1,6 @@
 package com.grupo3.medrem.activities;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,22 +10,17 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.grupo3.medrem.R;
 
-public class LoginActivity extends AppCompatActivity {
+public class DashboardActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_dashboard);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
-
-    public void onclickRegister(View view){
-        Intent intent = new Intent(this, RegisterActivity.class);
-        startActivity(intent);
     }
 }
--- app/src/main/java/com/grupo3/medrem/activities/RegisterActivity.java
+++ app/src/main/java/com/grupo3/medrem/activities/RegisterActivity.java
@@ -12,13 +12,13 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.grupo3.medrem.R;
 
-public class LoginActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -26,8 +26,8 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
-    public void onclickRegister(View view){
-        Intent intent = new Intent(this, RegisterActivity.class);
+    public void onclickLogin(View view){
+        Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
     }
 }
