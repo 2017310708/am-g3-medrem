e7eb907bcac2d2c3b7c58e6d7738ace088384076
Homero Percy Quispe Coaquira <percy91@gmail.com>
2025-06-11 17:43:59 -0500

    activitys modificadas
    login y register rediseñadas

--- app/src/main/java/com/grupo3/medrem/activities/RegisterActivity.java
+++ app/src/main/java/com/grupo3/medrem/activities/RegisterActivity.java
@@ -1,8 +1,10 @@
 package com.grupo3.medrem.activities;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,7 +14,11 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.grupo3.medrem.R;
 
+import java.util.Calendar;
+
 public class RegisterActivity extends AppCompatActivity {
+
+    private EditText etFechaNacimiento;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,10 +30,31 @@ public class RegisterActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        etFechaNacimiento = findViewById(R.id.register_fecha_nacimiento);
+        etFechaNacimiento.setOnClickListener(v -> mostrarDatePicker());
     }
 
     public void onclickLogin(View view){
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
     }
+    private void mostrarDatePicker() {
+        final Calendar calendario = Calendar.getInstance();
+        int año = calendario.get(Calendar.YEAR);
+        int mes = calendario.get(Calendar.MONTH);
+        int dia = calendario.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(
+                RegisterActivity.this,
+                (view, year, month, dayOfMonth) -> {
+                    String fecha = dayOfMonth + "/" + (month + 1) + "/" + year;
+                    etFechaNacimiento.setText(fecha);
+                },
+                año, mes, dia
+        );
+        datePickerDialog.show();
+    }
+
+
 }
